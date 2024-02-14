44f61fb2b6318e1aa78cec098c38a665bb08301c
Tania Boterman <taniaboterman@gmail.com>
2024-02-14 12:07:29 +0100

    regex added for email varification

--- models/catModel.js
+++ models/catModel.js
@@ -1,13 +1,26 @@
 import mongoose from 'mongoose';
-// const email regEX =
+// regex to validate email
+const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
 
 const catSchema = new mongoose.Schema({
-        last_name: { required: true, type: String },
-        first_name: { required: true, type: String },
-        email: { required: true, type: String }
-    }
-  );
+  last_name: { required: true, type: String },
+  first_name: { required: true, type: String },
+  email: { required: true, 
+            type: String,
+              validate: {
+              validator: function(v) {
+                return emailRegex.test(v);
+              },
+              message: props => `${props.value} is not a valid email!`
+            }
+          }  
+  });
 
 const Cat = mongoose.model('Cat', catSchema);
 
 export default Cat;
+
+/*1 - about regex (regular expressions)  it helps define 
+what an email should look like so user inputs an email
+it can also help find specific expressions in large texts and
+can help clean up text (whitespaces etc) */
